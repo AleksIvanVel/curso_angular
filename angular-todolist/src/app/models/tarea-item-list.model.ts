3a546fbefccb152678f612e570b16d1100533e17
AleksIvanVel <aleksvelazquez32@aragon.unam.mx>
2024-03-02 12:00:49 -0600

    prototipado y personalizado de vistas

--- angular-todolist/src/app/models/tarea-item-list.model.ts
+++ angular-todolist/src/app/models/tarea-item-list.model.ts
@@ -1,10 +1,11 @@
 export class TareaItemList{
     private selected:boolean = false;
+    public esp:string[];
 
     constructor( public tarea: string){
-        
+        this.esp = ['especificacion 1', 'especificacion 2'];
     }
-    
+
     isSelected():boolean{
         return this.selected;
     }
--- angular-todolist/src/app/tareas-pendientes/tareas-pendientes.component.ts
+++ angular-todolist/src/app/tareas-pendientes/tareas-pendientes.component.ts
@@ -1,11 +1,13 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { TareaItemList } from '../models/tarea-item-list.model';
+import { CommonModule } from '@angular/common';
+
 
 
 @Component({
   selector: 'app-tareas-pendientes',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './tareas-pendientes.component.html',
   styleUrl: './tareas-pendientes.component.css'
 })
