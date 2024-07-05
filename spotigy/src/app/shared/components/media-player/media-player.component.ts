c5b4f472027fc0354e862750c6d477ecd3f10c46
AleksIvanVel <aleksvelazquez32@aragon.unam.mx>
2024-07-05 11:38:51 -0600

    ADD: componentes compartidos

--- spotigy/src/app/shared/components/media-player/media-player.component.ts
+++ spotigy/src/app/shared/components/media-player/media-player.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrl: './media-player.component.css'
 })
 export class MediaPlayerComponent {
+  mockCover: any = {
+    cover: 'https://i.scdn.co/image/ab67616d0000b7345ca41b0d2352242c7c94bc',
+    album: 'Gioli & Assia',
+    name: 'BEBE (Oficial)'
+  }
 
 }
