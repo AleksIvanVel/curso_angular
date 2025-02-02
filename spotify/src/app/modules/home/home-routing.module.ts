94e5135906c19b610da9cc741a1df557b7c880ae
velalmanez <velalmanez@gmail.com>
2025-02-02 09:09:57 -0600

    UPDATE home page

--- spotify/src/app/modules/home/home-routing.module.ts
+++ spotify/src/app/modules/home/home-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomePageComponent } from './pages/home-page/home-page.component';
 
 const routes: Routes = [
+  //lazy loading de componentes cargados en el router outlet (hijo)
   {
-    path:'',
-    component: HomePageComponent
+    path:'tracks',
+    loadChildren:()=>import(`../tracks/tracks.module`).then(m=>m.TracksModule)
+  },
+  {
+    path:'favorites',
+    loadChildren:()=>import(`../favorites/favorites.module`).then(m=>m.FavoritesModule)
+  },
+  {
+    path:'history',
+    loadChildren:()=>import(`../history/history.module`).then(m=>m.HistoryModule)
   }
 ];
 
