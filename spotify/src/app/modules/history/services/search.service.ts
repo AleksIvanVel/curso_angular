2c6ebbf6711fbbf02b6a0cff3248fe6eed28a2c8
Aleks Velazquez <velalmanez@gmail.com>
2025-02-11 09:29:07 -0600

    ADD servicio de busqueda

---
+++ spotify/src/app/modules/history/services/search.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { enviroment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SearchService {
+  private readonly URL = enviroment.api
+  constructor(private http:HttpClient) { }
+
+
+  searchTrakcs$(term: string): Observable<any>{
+    return this.http.get(`${this.URL}/tracks?src=${term}`);
+  }
+}
